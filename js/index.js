d7c093d2d775879051ded0d53a5ade4d439394ca
Phong74R5 <lehongphong836@gmail.com>
2025-05-10 12:13:14 +0700

    fix

--- js/index.js
+++ js/index.js
@@ -1,38 +1,23 @@
-// Biến tham chiếu đến listener để quản lý
-let roomsRefListener = null;
-
 // Tải danh sách phòng
 function loadRooms() {
     const roomsBox = document.querySelector('.rooms-box');
-    // Tháo gỡ listener cũ nếu có
-    if (roomsRefListener) {
-        database.ref('rooms').off('value', roomsRefListener);
-    }
-
-    // Xóa nội dung cũ
-    roomsBox.innerHTML = '';
-
-    // Gắn listener mới
-    roomsRefListener = database.ref('rooms').on('value', (snapshot) => {
+    roomsBox.innerHTML = ''; // Xóa nội dung cũ trước khi render
+    database.ref('rooms').on('value', (snapshot) => {
         const rooms = snapshot.val();
         let totalDevices = 0;
-        roomsBox.innerHTML = ''; // Đảm bảo xóa nội dung trước khi render
-
         if (rooms) {
             for (const roomId in rooms) {
                 const room = rooms[roomId];
                 const deviceCount = Object.keys(room.devices || {}).length;
                 totalDevices += deviceCount;
-
-                const roomCard = document.createElement('a');
-                roomCard.href = `room.html?room=${room.name}&id=${roomId}`;
-                roomCard.className = 'room-card';
-                roomCard.innerHTML = `
-                    <i class="fa-solid ${room.icon || 'fa-bed-front'}"></i>
-                    <span>${room.name}</span>
-                    <span class="device-count">${deviceCount} thiết bị</span>
+                const roomCard = `
+                    <a href="room.html?room=${room.name}&id=${roomId}" class="room-card">
+                        <i class="fa-solid ${room.icon || 'fa-bed-front'}"></i>
+                        <span>${room.name}</span>
+                        <span class="device-count">${deviceCount} thiết bị</span>
+                    </a>
                 `;
-                roomsBox.appendChild(roomCard);
+                roomsBox.innerHTML += roomCard;
             }
         }
         document.querySelector('.all-devices .device-count').textContent = `${totalDevices} thiết bị`;
@@ -56,6 +41,7 @@ document.getElementById('addRoomForm')?.addEventListener('submit', (e) => {
     const roomIcon = document.querySelector('input[name="roomIcon"]:checked').value;
     addRoom(roomName, roomIcon);
     closeAddRoomModal();
+    loadRooms();
 });
 
 // Gọi hàm tải phòng khi trang được tải
